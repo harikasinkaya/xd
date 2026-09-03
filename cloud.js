9200fa17e5d9d0cf4736f101c480eddffb6432cb
Kaya <sunucubudur@gmail.com>
2026-09-03 03:11:49 +0300

    Create cloud.js

---
+++ cloud.js
@@ -0,0 +1,19 @@
+<script src="three.r134.min.js"></script>
+<script src="vanta.clouds.min.js"></script>
+<script>
+VANTA.CLOUDS({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  backgroundColor: 0x0,
+  skyColor: 0x0,
+  cloudColor: 0x555561,
+  cloudShadowColor: 0x0,
+  sunColor: 0x111111,
+  sunGlareColor: 0x0,
+  sunlightColor: 0xf0e0e
+})
+</script>
